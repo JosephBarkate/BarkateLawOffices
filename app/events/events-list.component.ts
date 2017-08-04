5b819bb896fafae71952a6af45f4a5ffe5dd7f30
US\joe.barkate <joe.barkate@schwab.com>
2017-08-04 15:18:45 -0500

    Half way through 6

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
 @Component({
-	selector: 'events-list',
 	templateUrl: 'app/events/events-list.component.html'
 	
 })
